17716091cb80ae7ed9019ce0d2fd6e18d500b766
Aaron <a.rcottrill521@gmail.com>
2021-04-05 12:00:15 -0400

    Get file uploads working; no filter yet

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const bodyParser = require('body-parser')
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -7,9 +9,38 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
 });
+
+var upload = multer({ storage: storage });
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/uploadfile', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    return next(error)
+  }
+  res.send(
+    {
+      name: file.filename,
+      type: file.mimetype,
+      size: file.size
+    }
+  )
+})
+
+
 
 
 
